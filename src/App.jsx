6add7fa596b037979dd2c30aa1fc71ad91f350e0
Rifqi Hasan <hasanrifqi0905@gmail.com>
2021-07-29 11:36:01 +0700

    membuat portofolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,48 @@
 import React from 'react';
 import './App.css';
+import Backgroundimage from "./assets/bg.jpg";
+import FotoProfil from "./assets/pp.JPG"
 
 
 function App() {
   return (
    <React.Fragment>
-     <div className="grid grid-cols-7 h-screen w-screen gap-y-1 gap-x-1">
-       <div className="bg-red-500 col-span-1  "></div>
-       <div className="bg-green-500 col-span-6 overflow-auto  ">
-         <div className="h-12 bg-yellow-500">1</div>
-         <div className="h-full flex items-center justify-center">
-         <form className="pt-10 px-10">
-           <label className="font-bold text-xl" htmlFor="username">username</label>
-           <input className="border bg-gray-100 w-full px-10 py-3 hover:bg-blue-100" type="text"/>
-         </form>
+     <div className="antialiased relative text-gray-600">
+       {/* ============================================ */}
+       <div className="absolute w-full  min-h-screen">
+         <div
+          className="absolute top-0 w-full h-1/2 bg-cover bg-bottom" 
+          style={{backgroundImage : `url(${Backgroundimage})`}}
+         ></div>
+         <div className="absolute
+         z-20 bottom-10 right-0 left-0 inline-flex space-x-20 justify-center uppercase font-bold text-gray-600"
+         > 
+            <a className="hover:text-blue-600" href="#services">services</a>
+            <a className="hover:text-blue-600" href="#work">work</a>
+            <a className="hover:text-blue-600" href="#contact">contact</a>
          </div>
        </div>
-
-       
+        {/* ============================================ */}
+        {/* card identitas */}
+        <div className="relative z-10 flex justify-center items-center min-h-screen h-auto ">
+          <div className="max-w-4xl relative">
+          <div className="absolute inset-0 transform -skew-x-4  z-10 -rotate-6  w-full max-w-4xl p-12 shadow-lg rounded-lg bg-gradient-to-r from-blue-500 to-red-500"></div>
+          <div className="relative z-20 flex bg-white justify-between w-full max-w-4xl p-12 shadow-lg rounded-lg">
+            <div className="flex flex-col space-y-6 py-6 justify-between">
+            <div>
+              <h2 className="text-lg">HELLO I AM Rifqi</h2>
+              <h1 className="text-4xl font-bold text-gray-700">Rifqi Hasan</h1>
+            </div>
+            <p className="text-md leading-relaxed">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos optio iusto delectus laboriosam tempora repellendus veritatis sapiente, minima nulla magni repellat neque praesentium odit, assumenda magnam expedita impedit porro in.
+            </p>
+            </div>
+            <img className="w-64 h-64 rounded-full border-white shadow-lg flex-shrink-0" src={FotoProfil} alt="pp.JPG" />
+          </div>
+          </div>
+        </div>
+        {/* card identitas */}
+ 
      </div>
    </React.Fragment>
   );
